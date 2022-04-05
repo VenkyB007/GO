85d7c40b212ea1706cd7ce53950c0f91e04ab89c
VenkyB007 <102458222+VenkyB007@users.noreply.github.com>
2022-04-05 15:29:23 +0530

    Add files via upload

---
+++ Doctor_Appointment_App/go.mod
@@ -0,0 +1,6 @@
+module github.com/VenkyB007/go/Doctor_Appointment_App
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
---
+++ Doctor_Appointment_App/route/routes.go
@@ -0,0 +1,18 @@
+package route
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/vikasbolla/DoctorApplication/Doctor_application/service"
+)
+
+func Start() {
+	myRouter := mux.NewRouter()
+	myRouter.HandleFunc("/login", service.Login).Methods("POST")
+	myRouter.HandleFunc("/getall", service.DoctorDetails).Methods("GET")
+	myRouter.HandleFunc("/postavailability", service.PostAvailability).Methods("POST")
+	myRouter.HandleFunc("/getsuitableappointment", service.GetSuitableAppointment).Methods("POST")
+	log.Fatal((http.ListenAndServe(":9009", myRouter)))
+}
