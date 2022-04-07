5c597c1213ed2a78305afb2f0b93932ba27be34d
VenkyB007 <102458222+VenkyB007@users.noreply.github.com>
2022-04-07 14:46:22 +0530

    Delete Hiring_Application directory

--- Hiring_Application/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/VenkyB007/go/Hiring_Application
-
-go 1.18
-
-require github.com/gorilla/mux v1.8.0
--- Hiring_Application/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/VenkyB007/go/Hiring_Application/routes"
-
-func main() {
-	routes.Handle()
-}
